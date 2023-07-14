47d394757f0fdbd8bbbf96b287732d852fe65724
Eka Broladze <broladze.ekaterine@gmail.com>
2023-07-15 01:20:36 +0400

    correction in primitive types

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const yearOfBirth = 1990
 let currentYear = 2023
 
 console.log(`My name is ${myName} I am ${currentYear-yearOfBirth} years old and my hobby is ${myHobby} `)
-console.log("My name is " + myName, "I am " + (currentYear-yearOfBirth) + " years old amd my hobby is " + myHobby)
+console.log("My name is " + myName, "I am " + (currentYear-yearOfBirth) + " years old and my hobby is " + myHobby)
 
 //Primitive Types
 
 let num = 137;
-let word = "word"
-let example; //undefined
-let result = true; //boolean
+const userName ="eka";
+let example; 
+const isUsernameCorrect = false; 
 let none = null;
-console.log("the types are: ", typeof 137,"string", example, result, null )
+
+console.log(typeof num, typeof userName, typeof example, typeof isUsernameCorrect, typeof none);
