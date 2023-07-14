634a7cd19ee23ccc644cf69caa2ec859b58a689c
Eka Broladze <broladze.ekaterine@gmail.com>
2023-07-15 00:47:05 +0400

    added html & js

---
+++ app.js
@@ -0,0 +1,16 @@
+const myName = "Eka"
+const myHobby = "painting"
+const yearOfBirth = 1990
+let currentYear = 2023
+
+console.log(`My name is ${myName} I am ${currentYear-yearOfBirth} years old and my hobby is ${myHobby} `)
+console.log("My name is " + myName, "I am " + (currentYear-yearOfBirth) + " years old amd my hobby is " + myHobby)
+
+//Primitive Types
+
+let num = 137;
+let word = "word"
+let example; //undefined
+let result = true; //boolean
+let none = null;
+console.log("the types are: ", typeof 137,"string", example, result, null )
